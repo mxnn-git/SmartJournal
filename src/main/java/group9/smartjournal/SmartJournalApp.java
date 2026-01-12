4dcdc0d9c6a149a53f5d7a115f95259ee11ca189
Ibragim Abdulaev <25054912@siswa.um.edu.my>
2026-01-12 08:27:12 +0800

    Init

--- src/main/java/group9/smartjournal/SmartJournalApp.java
+++ src/main/java/group9/smartjournal/SmartJournalApp.java
@@ -44,7 +44,7 @@ public class SmartJournalApp {
         System.out.println("\n=== Welcome to Smart Journaling System ===");
 
         while (currentUser == null) {
-            System.out.println("\n1. Login");
+            System.out.println("1. Login");
             System.out.println("2. Register New Account");
             System.out.println("3. Exit");
             System.out.print("Choose option: ");
